ae8974c9e56a4b9ef13c307bda394db1fd1bdc97
Ricky Reusser <rsreusser@gmail.com>
2015-07-24 20:03:28 -0400

    Sigh. Fix equations

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,9 @@ var gulp = require('gulp')
   , path = require('path')
 
 function giturl(rel) {
-  return 'https://rawgit.com/scijs/ndarray-band/master/' + rel
+  //return 'https://rawgit.com/scijs/ndarray-band/master/' + rel
   //return 'https://cdn.rawgit.com/rreusser/gulp-markdown-equations/master/' + rel
-  //return rel
+  return rel
 }
 
 gulp.task('mdtex',function() {
@@ -20,8 +20,8 @@ gulp.task('mdtex',function() {
   var mdFilter = filter('*.md')
   var sub = eqSub({
     defaults: {
-      display: { margin: '1pt 5pt', fontSize: '12pt' },
-      inline: { margin: '0 2pt 0pt 1pt', fontSize: '12pt' }
+      display: { margin: '1pt 5pt' },
+      inline: { margin: '1pt 1t 1pt -4pt' }
     }
   })
 
@@ -30,11 +30,12 @@ gulp.task('mdtex',function() {
 
     .pipe(texFilter)
     .pipe(latex())
-    .pipe(pdftocairo({format: 'svg'}))
+    //.pipe(pdftocairo({format: 'svg'}))
+    .pipe(pdftocairo({format: 'png', resolution: 288}))
     .pipe(gulp.dest('docs/images'))
     .pipe(tap(function(file) {
       sub.completeSync(file,function(cb) {
-        var img = '<img alt="'+this.alt+'" valign="middle" width="' + this.width*1.6 + '" height="' + this.height*1.6 + '" src="'+giturl(this.path)+'">'
+        var img = '<img alt="'+this.alt+'" valign="middle" width="' + this.width/2 + '" height="' + this.height/2 + '" src="'+giturl(this.path)+'">'
         return this.display ? '\n\n<p align="center">'+img+'</p>\n\n' : img
       })
     }))
@@ -42,6 +43,5 @@ gulp.task('mdtex',function() {
 
     .pipe(mdFilter)
     .pipe(gulp.dest('./'))
-    .pipe(mdFilter.restore())
 
 })
